40b77eb6d62c564832e0f07ed9d7fe3d64878678
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-24 15:25:33 -0600

    Removed the href='' prop from all the navigation options

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,23 +4,13 @@ import * as React from "react";
 const NavBarComponent: React.FC = () => {
   return (
     <Navbar className="bg-orange-500" fluid={true}>
-      <Navbar.Brand href="#">
-        <DarkThemeToggle className="text-white" />
-      </Navbar.Brand>
+      <DarkThemeToggle className="text-white" />
       <Navbar.Toggle className="font-bold text-white" />
       <Navbar.Collapse>
-        <Navbar.Link className=" font-bold text-white" href="#">
-          How We Work
-        </Navbar.Link>
-        <Navbar.Link className="font-bold text-white" href="#">
-          Services
-        </Navbar.Link>
-        <Navbar.Link className="font-bold text-white" href="#">
-          Free Quote
-        </Navbar.Link>
-        <Navbar.Link className="font-bold text-white" href="#">
-          Contact
-        </Navbar.Link>
+        <Navbar.Link className=" font-bold text-white">How We Work</Navbar.Link>
+        <Navbar.Link className="font-bold text-white">Services</Navbar.Link>
+        <Navbar.Link className="font-bold text-white">Free Quote</Navbar.Link>
+        <Navbar.Link className="font-bold text-white">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );

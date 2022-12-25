d4a9ef2c04eb38ea31fb7f135d391c6733d3be9c
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 17:39:30 -0600

    Initialize the list of items

--- data/store.ts
+++ data/store.ts
@@ -16,12 +16,65 @@ interface ItemsState {
 }
 
 const useStore = create<ItemsState>((_set) => ({
-  items: [],
+  // initialize the state
+  items: [
+    {
+      icon: "bed",
+      description: "Beds",
+      m2: 1.2,
+    },
+    {
+      icon: "refrigerator",
+      description: "Regrigerador",
+      m2: 1,
+    },
+    {
+      icon: "forniture",
+      description: "Forniture",
+      m2: 0.5,
+    },
+    {
+      icon: "oven",
+      description: "Oven",
+      m2: 0.6,
+    },
+    {
+      icon: "sofa",
+      description: "Sofa",
+      m2: 1.5,
+    },
+    {
+      icon: "tv",
+      description: "TV",
+      m2: 0.25,
+    },
+    {
+      icon: "washer-dryer",
+      description: "Washer-dryer",
+      m2: 0.5,
+    },
+    {
+      icon: "dining",
+      description: "Dining",
+      m2: 2,
+    },
+    {
+      icon: "desk",
+      description: "Desk",
+      m2: 0.75,
+    },
+    {
+      icon: "wardrobe",
+      description: "Wardrobe",
+      m2: 3.2,
+    },
+  ],
   totalItems: 0,
   totalM2: 0,
   subtotal: 0,
   total: 0,
   dueToday50: 0,
+  // initialize the state
 }));
 
 export { useStore };

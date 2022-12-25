0c2d2da2d8b9ae3224a0bc489e5209da4d5b3569
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-24 18:23:36 -0600

    Added the <FooterComponent /> to the index.tsx page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 
+import { FooterComponent } from "../components/Footer";
 import { NavBarComponent } from "../components/NavBar";
 
 function IndexPage() {
   return (
     <React.Fragment>
       <NavBarComponent />
+      <div className="border-2 flex-1 h-screen" />
+      <FooterComponent />
     </React.Fragment>
   );
 }

83413a1214935f78bbf8d8ad2b7bee8bcc47ccf4
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 12:15:01 -0600

    Added other to <p /> with the classes to handle responsivness

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,12 @@ function IndexPage() {
         <p className="text-center font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl dark:text-white">
           What items to store?
         </p>
+        <p className="text-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl dark:text-white">
+          Select which items you wish to store before moving to
+        </p>
+        <p className="text-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl dark:text-white">
+          your new home. We'll keep'em safe!
+        </p>
       </div>
       <FooterComponent />
     </React.Fragment>

97ffc11e77afabcb115e1edc7d8b9f234d0d7c7e
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 13:26:08 -0600

    Added the <div /> component that will contain the list of elements, all in a responsive way

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { FooterComponent } from "../components/Footer";
+import { ItemComponent } from "../components/Item";
 import { NavBarComponent } from "../components/NavBar";
 
 function IndexPage() {
@@ -18,6 +19,18 @@ function IndexPage() {
           <p className="text-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl dark:text-white">
             your new home. We'll keep'em safe!
           </p>
+        </div>
+        <div className="mx-5 md:mx10 lg:mx-20 xl:mx-30 2xl:mx-40 grid grid-cols-1 sm:grid-cols-2:mx-20 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-5">
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
+          <ItemComponent />
         </div>
       </div>
       <FooterComponent />

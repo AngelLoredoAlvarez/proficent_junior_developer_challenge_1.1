e29f7aa1d3748c92d94e699a397a72c7e7f7188f
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 14:40:44 -0600

    Added all the code to generate the inputs that will change the state according the selected item

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,4 +1,4 @@
-import { Card } from "flowbite-react";
+import { Button, Card, TextInput } from "flowbite-react";
 import * as React from "react";
 
 const ItemComponent: React.FC = () => {
@@ -8,7 +8,15 @@ const ItemComponent: React.FC = () => {
       <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
         Beds
       </p>
-      <div className="border-2" />
+      <div className="flex">
+        <span className="inline-flex items-center text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+          <Button className="bg-orange-500 dark:bg-slate-700">-</Button>
+        </span>
+        <TextInput className="rounded-none bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <span className="inline-flex items-center border border-r-0 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+          <Button className="bg-orange-500 dark:bg-slate-700">+</Button>
+        </span>
+      </div>
     </Card>
   );
 };

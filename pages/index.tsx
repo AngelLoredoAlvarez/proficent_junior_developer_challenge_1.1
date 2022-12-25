280d261e9a291f3eef78b8259adf852d0156cebd
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 12:03:12 -0600

    Added the <p /> component with a responsive text

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,11 @@ function IndexPage() {
   return (
     <React.Fragment>
       <NavBarComponent />
-      <div className="border-2 flex-1 h-screen" />
+      <div className="border-2 flex-1 h-screen">
+        <p className="text-center font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
+          What items to store?
+        </p>
+      </div>
       <FooterComponent />
     </React.Fragment>
   );

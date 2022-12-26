2c7f3279b5a8df91d3471a4b3cad304ef55750da
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-25 23:17:15 -0600

    Destructured all the props to be rendered from the custom useStore() hook

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,7 +1,22 @@
 import { Button, Card } from "flowbite-react";
 import * as React from "react";
+import shallow from "zustand/shallow";
+
+import { useStore } from "../data/store";
 
 const SummaryComponent: React.FC = () => {
+  const { totalItems, totalM2, subtotal, tax, total, dueToday50 } = useStore(
+    (state) => ({
+      totalItems: state.totalItems,
+      totalM2: state.totalM2,
+      subtotal: state.subtotal,
+      tax: state.tax,
+      total: state.total,
+      dueToday50: state.dueToday50,
+    }),
+    shallow
+  );
+
   return (
     <Card>
       <div className="grid grid-cols-2 gap-10">
@@ -9,37 +24,49 @@ const SummaryComponent: React.FC = () => {
           Total Items:
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          12
+          {totalItems}
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Total M2:
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          8.55
+          {totalM2}
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Subtotal:
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          $ 1,710.00
+          {subtotal.toLocaleString("es-MX", {
+            style: "currency",
+            currency: "MXN",
+          })}
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Tax:
         </p>
         <p className="text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          $ 237.00
+          {tax.toLocaleString("es-MX", {
+            style: "currency",
+            currency: "MXN",
+          })}
         </p>
         <p className="font-bold text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Total:
         </p>
         <p className="font-bold text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          $ 1,983.60
+          {total.toLocaleString("es-MX", {
+            style: "currency",
+            currency: "MXN",
+          })}
         </p>
         <p className="font-bold text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Due Today 50%:
         </p>
         <p className="font-bold text-gray-900 dark:text-white sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          $ 991.80
+          {dueToday50.toLocaleString("es-MX", {
+            style: "currency",
+            currency: "MXN",
+          })}
         </p>
         <div className="col-span-2">
           <Button className="flex w-full bg-orange-500 hover:bg-orange-400 dark:bg-slate-700 dark:hover:bg-slate-900">

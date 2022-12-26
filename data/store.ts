6467366b535ece8da632ae77f6d2c6120acff772
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-12-26 00:43:43 -0600

    Moved the Initial State in the items array outside of the initializar of the state, with this change, we can reset the state of the array of items when the <Button>Clear</Button> button is pressed in the <SummaryComnent /> component

--- data/store.ts
+++ data/store.ts
@@ -20,70 +20,72 @@ interface ItemsState {
   clearItems: () => void;
 }
 
+const initialState: Item[] = [
+  {
+    icon: "bed",
+    description: "Beds",
+    quantity: 0,
+    m2: 1.2,
+  },
+  {
+    icon: "refrigerator",
+    description: "Regrigerador",
+    quantity: 0,
+    m2: 1,
+  },
+  {
+    icon: "furniture",
+    description: "Forniture",
+    quantity: 0,
+    m2: 0.5,
+  },
+  {
+    icon: "oven",
+    description: "Oven",
+    quantity: 0,
+    m2: 0.6,
+  },
+  {
+    icon: "sofa",
+    description: "Sofa",
+    quantity: 0,
+    m2: 1.5,
+  },
+  {
+    icon: "tv",
+    description: "TV",
+    quantity: 0,
+    m2: 0.25,
+  },
+  {
+    icon: "washer-dryer",
+    description: "Washer-dryer",
+    quantity: 0,
+    m2: 0.5,
+  },
+  {
+    icon: "dining",
+    description: "Dining",
+    quantity: 0,
+    m2: 2,
+  },
+  {
+    icon: "desk",
+    description: "Desk",
+    quantity: 0,
+    m2: 0.75,
+  },
+  {
+    icon: "wardrobe",
+    description: "Wardrobe",
+    quantity: 0,
+    m2: 3.2,
+  },
+];
+
 const useStore = create<ItemsState>((set) => ({
   // initialize the state
-  items: [
-    {
-      icon: "bed",
-      description: "Beds",
-      quantity: 0,
-      m2: 1.2,
-    },
-    {
-      icon: "refrigerator",
-      description: "Regrigerador",
-      quantity: 0,
-      m2: 1,
-    },
-    {
-      icon: "furniture",
-      description: "Forniture",
-      quantity: 0,
-      m2: 0.5,
-    },
-    {
-      icon: "oven",
-      description: "Oven",
-      quantity: 0,
-      m2: 0.6,
-    },
-    {
-      icon: "sofa",
-      description: "Sofa",
-      quantity: 0,
-      m2: 1.5,
-    },
-    {
-      icon: "tv",
-      description: "TV",
-      quantity: 0,
-      m2: 0.25,
-    },
-    {
-      icon: "washer-dryer",
-      description: "Washer-dryer",
-      quantity: 0,
-      m2: 0.5,
-    },
-    {
-      icon: "dining",
-      description: "Dining",
-      quantity: 0,
-      m2: 2,
-    },
-    {
-      icon: "desk",
-      description: "Desk",
-      quantity: 0,
-      m2: 0.75,
-    },
-    {
-      icon: "wardrobe",
-      description: "Wardrobe",
-      quantity: 0,
-      m2: 3.2,
-    },
-  ],
+  items: [...initialState],
   totalItems: 0,
   totalM2: 0,
   subtotal: 0,
@@ -177,7 +179,7 @@ const useStore = create<ItemsState>((set) => ({
   // function that is called when the <Button>Clear</Button> is pressed
   clearItems: () => {
     set(() => ({
-      items: [],
+      items: [...initialState],
       totalItems: 0,
       totalM2: 0,
       subtotal: 0,
